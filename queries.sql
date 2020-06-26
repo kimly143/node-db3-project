bf302904f0b686ae3d998f5da26918605723a1b0
Kim Ly Buck <kim.3993@gmail.com>
2020-06-25 19:39:12 -0500

    first query

--- queries.sql
+++ queries.sql
@@ -1,7 +1,11 @@
 -- Multi-Table Query Practice
 
--- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
 
+-- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
+SELECT P.ProductName, C.CategoryName
+FROM Product AS P
+INNER JOIN Category AS C ON C.Id = P.CategoryId
+;
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
