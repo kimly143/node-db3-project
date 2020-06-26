fda72c3971ff8d332bf0c8c4ce78f8316c5f5102
Kim Ly Buck <kim.3993@gmail.com>
2020-06-25 19:57:48 -0500

    second query

--- queries.sql
+++ queries.sql
@@ -7,7 +7,9 @@ FROM Product AS P
 INNER JOIN Category AS C ON C.Id = P.CategoryId
 ;
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
-
+SELECT O.id, S.CompanyName
+FROM "Order" as O
+INNER JOIN Shipper as S ON S.Id = O.ShipVia WHERE O.OrderDate < '2012-08-09'; 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
